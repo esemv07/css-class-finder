6ab43bb5068a68adf036ab765aa2686e65fabdf5
esemv07 <ruby.semaganda@gmail.com>
2026-04-25 16:03:57 +1000

    Quick pick CSS files in current workspace and open selected file

--- extension.js
+++ extension.js
@@ -1,33 +1,56 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "css-class-finder" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('css-class-finder.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from CSS Class Finder!');
 	});
 
-	context.subscriptions.push(disposable);
+	const disposable2 = vscode.commands.registerCommand('css-class-finder.transferSelectedClasses', async function () {
+		/* const editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			vscode.window.showWarningMessage('No active editor found.');
+			return
+		}
+
+		const selection = editor.selection;
+		const selectedText = editor.document.getText(selection);
+
+		if (!selectedText || selectedText.trim().length === 0) {
+			vscode.window.showWarningMessage('No text selected.');
+			return
+		}
+
+		const document = editor.document; */
+
+		const files = await vscode.workspace.findFiles('**/*.css', '**/node-modules/**', 10);
+		const pickFiles = files.map(file => ({
+			label: vscode.workspace.asRelativePath(file),
+			uri: file
+		}));
+
+		const fileSelection = await vscode.window.showQuickPick(pickFiles, {
+			placeHolder: 'Select a stylesheet to transfer classes to',
+			matchOnDetail: true
+		});
+
+		if (fileSelection) {
+			const document = await vscode.workspace.openTextDocument(fileSelection.uri);
+    		await vscode.window.showTextDocument(document);
+		}
+	});
+
+	context.subscriptions.push(disposable, disposable2);
 }
 
-// This method is called when your extension is deactivated
+
 function deactivate() {}
 
 module.exports = {
