0dacd74f605995907560c15e216fb5b72bdec1e6
esemv07 <ruby.semaganda@gmail.com>
2026-04-27 16:08:01 +1000

    Add automatically transfering classes to CSS file on HTML file save

--- extension.js
+++ extension.js
@@ -6,7 +6,9 @@ const vscode = require('vscode');
 function activate(context) {
 
 	let allClasses = new Array();
+	let transferedClasses = new Array();
 	let htmlFile = "";
+	let cssFile = "";
 
 	console.log('Congratulations, your extension "css-class-finder" is now active!');
 
@@ -138,10 +140,10 @@ function activate(context) {
 	})
 
 	const saveListener = vscode.workspace.onDidSaveTextDocument(async document => {
+		const fileContent = await vscode.workspace.fs.readFile(document.uri);
+		const fileText = new TextDecoder().decode(fileContent);
+		
 		if (vscode.workspace.asRelativePath(document.uri) == htmlFile) {
-			const fileContent = await vscode.workspace.fs.readFile(document.uri);
-			const fileText = new TextDecoder().decode(fileContent);
-
 			let classes = [];
 			const reg = /class=["']([^"']+)["']/gm;
 			let match;
@@ -157,9 +159,74 @@ function activate(context) {
 			}
 			vscode.commands.executeCommand('editor.action.triggerSuggest');
 		}
+
+		if ((vscode.workspace.asRelativePath(document.uri).includes(".html")) && cssFile.length > 0) {
+			let classes = [];
+			const reg = /class=["']([^"']+)["']/gm;
+			let match;
+
+			while ((match = reg.exec(fileText)) !== null) {
+				const indClass = match[1].split(/\s+/);
+				classes.push(...indClass);
+			}
+			const uniqueClasses = [...new Set(classes)];
+			if (uniqueClasses.length == 0) {
+				vscode.window.showWarningMessage('No classes in current file.');
+				return
+			}
+
+			const newClasses = uniqueClasses.filter(indClass => !transferedClasses.includes(indClass));
+
+			const document = await vscode.workspace.openTextDocument(vscode.Uri.parse(cssFile));
+			const edit = new vscode.WorkspaceEdit()
+			let stylesText = "";
+			newClasses.forEach( function(newClass) {
+				transferedClasses.push(newClass);
+				stylesText += `
+.${newClass} {
+	/* insert ${newClass} styles */
+}\n\n`
+			});
+			const lastLine = document.lineAt(document.lineCount - 1);
+			const position = new vscode.Position(lastLine.lineNumber, lastLine.text.length);
+			edit.insert(vscode.Uri.parse(cssFile), position, (stylesText));
+			await vscode.workspace.applyEdit(edit);
+		}
 	})
 
-	context.subscriptions.push(disposable2, disposable3, provider, saveListener);
+	const disposable4 = vscode.commands.registerCommand('css-class-finder.autoTransferClasses', async function () {
+		const editor = vscode.window.activeTextEditor;
+
+		if (!editor) {
+			vscode.window.showWarningMessage('No active editor found.');
+			return
+		} else if (!editor.document.uri.toString().includes('.html')) {
+			vscode.window.showWarningMessage('Current file not of .html format.');
+			return
+		}
+
+		const files = await vscode.workspace.findFiles('**/*.css', '**/node_modules/**', 10);
+		if (files.length === 0) {
+			vscode.window.showWarningMessage('No stylesheets to select.')
+			return
+		}
+		const pickFiles = files.map(file => ({
+			label: vscode.workspace.asRelativePath(file),
+			uri: file
+		}));
+
+		const fileSelection = await vscode.window.showQuickPick(pickFiles, {
+			placeHolder: 'Select a stylesheet to transfer classes to',
+			matchOnDetail: true
+		});
+
+		if (fileSelection) {
+			cssFile = fileSelection.uri.toString();
+			vscode.window.showInformationMessage('Save the current HTML file to transfer any new classes.')
+		}
+	})
+
+	context.subscriptions.push(disposable2, disposable3, provider, saveListener, disposable4);
 }
 
 
